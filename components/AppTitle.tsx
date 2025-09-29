d16f0abffec57de7ad55880092477b45bc8fa157
Muhammad Ameed Ghouri <ameedghouri45@gmail.com>
2025-09-29 16:21:19 +0500

    Update dynamic app titles

--- components/AppTitle.tsx
+++ components/AppTitle.tsx
@@ -1,13 +1,25 @@
 "use client";
 
+import { APP_ROUTES } from "@/utils/app_routes";
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import { usePathname } from "next/navigation";
 
 const AppTitle: React.FC = () => {
+  const pathname = usePathname();
+
+  const getTitle = () => {
+    if (pathname === APP_ROUTES.DASHBOARD) return "Dashboard";
+    if (pathname === APP_ROUTES.CALENDAR) return "Calendar";
+    if (pathname === APP_ROUTES.PROJECTS) return "Projects";
+    if (pathname === APP_ROUTES.DOCUMENTS) return "Documents";
+    if (pathname === APP_ROUTES.PAYMENTS) return "Payments";
+    return "App Title";
+  };
+
   return (
     <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
       <Typography variant="h6" noWrap component="div">
-        Dashboard
+        {getTitle()}
       </Typography>
     </Box>
   );
