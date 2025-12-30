ac3f546c40c7aa8ea8396f9d6e2410343d7d4dc9
Syed-Yarooq-Hussain <syed.yarooq@gmail.com>
2025-12-30 18:18:03 +0100

    fix 2

--- types/signup-form.d.ts
+++ types/signup-form.d.ts
@@ -17,8 +17,8 @@ export interface ISignUpConsultantForm {
   confirmPassword: string;
   city?: string;
   country?: string;
-  coreModule: string[];
-  otherModule: string[];
+  coreModule: any;
+  otherModule: any;
   experience: number;
   rate: number;
   availableHours: number;
