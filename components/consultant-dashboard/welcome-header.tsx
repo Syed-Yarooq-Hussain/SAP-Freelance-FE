63c53dd13a34a926f5f45a592c1462cb09a85a72
wasiq-97 <syed.wasiq1797@gmail.com>
2026-04-02 00:27:49 +0500

    minor fix

--- components/consultant-dashboard/welcome-header.tsx
+++ components/consultant-dashboard/welcome-header.tsx
@@ -89,7 +89,7 @@ export function WelcomeHeader() {
         </div>
 
         {/* Working Clock */}
-        <div className="flex items-center justify-center gap-2 bg-brand-blue rounded-xl px-3 py-2 border border-gray-200 shadow-sm w-full lg:w-auto ">
+        <div className="flex items-center justify-center gap-2 bg-brand-blue rounded-xl px-4 md:px-7 py-2 border border-gray-200 shadow-sm w-full lg:w-auto ">
           <Clock className="w-8 h-8 flex-shrink-0 text-white" />
           <div className="text-center">
             <p className="text-xs font-thin text-white tracking-wide">GMT TIME</p>
