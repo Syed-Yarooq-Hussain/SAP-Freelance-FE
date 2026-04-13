acc2b8405228f41023488b05c3303f2819402710
Syed-Yarooq-Hussain <syed.yarooq@gmail.com>
2026-04-13 22:29:50 +0200

    cal

--- app/admin/module/ModuleTable.tsx
+++ app/admin/module/ModuleTable.tsx
@@ -34,7 +34,6 @@ export default function ModuleTable({ onEdit }: any) {
   const renderRows: any = (nodes: any[], level = 0) => {
     return nodes.map((node) => (
       <>
-      
         <tr
           key={node.id}
           className="border-t hover:bg-gray-50 transition"
