bc2c7af8f63c1ebcc39d08c1b714354d852e5300
Syed-Yarooq-Hussain <syed.yarooq@gmail.com>
2026-05-04 21:02:05 +0200

    profile change missing stuff 2

--- middleware.ts
+++ middleware.ts
@@ -47,6 +47,7 @@ export default auth((req) => {
 
   if (session) {
     const role = session.user?.role;
+    
     const isConsultantRoute = pathname.startsWith("/consultant");
     const isClientRoute = pathname.startsWith("/client");
     const isAdminRoute = pathname.startsWith("/admin");
