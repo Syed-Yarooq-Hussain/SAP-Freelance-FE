ef193360c222de77433e7e9450d05cbee3b39053
Syed-Yarooq-Hussain <syed.yarooq@gmail.com>
2026-06-09 17:58:14 +0200

    deploye main

--- components/AssignedRolePopup.tsx
+++ components/AssignedRolePopup.tsx
@@ -126,7 +126,6 @@ export function AssignedRolePopup({
           />
         ))}
       </Box>
-
       <Typography sx={{ fontWeight: 700, fontSize: "1rem", mb: 1 }}>
         Role
       </Typography>
