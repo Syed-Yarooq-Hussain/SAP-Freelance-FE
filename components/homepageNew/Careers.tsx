2ccd56bacb310ab18cfaf0136e0f0bbb54b43e4a
wasiq-97 <syed.wasiq1797@gmail.com>
2026-06-11 13:18:37 +0500

    update home page banner with new image

--- components/homepageNew/Careers.tsx
+++ components/homepageNew/Careers.tsx
@@ -102,7 +102,7 @@ export default function Careers({ onSignUpClick }: CareersProps) {
   return (
     <section 
       id="join-consultant"
-      className="py-16 md:py-12 bg-hero-gradient relative scroll-mt-24">
+      className="py-8 md:py-12 bg-hero-gradient relative scroll-mt-24">
       <div className="absolute top-0 left-[-10%] w-1/2 h-full z-0">
         <Image
           src="/images/homepage/shade-blue.png"
