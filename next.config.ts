b6eddf9a8c2f135147a89de188671c212991a966
Syed-Yarooq-Hussain <syed.yarooq@gmail.com>
2026-06-20 09:42:21 +0200

    dockers

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     domains: [
       "sap-freelancer-platform.s3.eu-central-1.amazonaws.com",
